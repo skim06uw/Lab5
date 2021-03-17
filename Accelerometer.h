16aa4d4e3209b28efbf927d125a1d0c52d5472ff
gj-uwece <77952836+gj-uwece@users.noreply.github.com>
2021-03-16 17:05:28 -0700

    Add files via upload

--- Accelerometer.h
+++ Accelerometer.h
@@ -6,14 +6,30 @@
 #include <Arduino.h>
 #include "Accelerometer.h"
 
+#define PI 3.14159265
+#define DELTA_T 0.001
+#define X_PIN A10
+#define Y_PIN A11
+#define Z_PIN A12
+
+typedef struct xyzVector {
+    float* x;
+    float* y;
+    float* z;
+};
+
 typedef struct accelerometerTaskData {
-    float* xRelPosition;
-    float* yRelPosition;
-    float* zRelPosition;
+    xyzVector* staticAngle;
+    xyzVector* position;
+    xyzVector* velocity;
+    xyzVector* acceleration;
     float* totalDistance;
-    float* staticAngle;
     bool* displayAccelFlag;
 } accelerometerData;
 
 void accelerometerTask(void*);
+float avgAccel(float* a, int pin);
+float avgVelocity(float* v, float* a);
+float displacement(float* position, float vBar);
+
 #endif
